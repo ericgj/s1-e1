f8b355b7a5db2bbfb4149896ac2d0d034321dc90
ericgj <ericgj72@gmail.com>
2010-09-03 21:07:18 -0400

    confirmed connection to Twitter via em-http-request and oauth (twitter_http)

--- scratch/ymlp_http.rb
+++ scratch/ymlp_http.rb
@@ -1,28 +1,25 @@
 require 'rubygems'
-require 'eventmachine'
+require 'em-http-request'
 
 
 EM.run {
 
-  conn = EM::Protocols::HttpClient2.connect( 
-            :host => 'ymlp.com',
-            :port => 80
-         )
+  http = EventMachine::HttpRequest.new( 
+            'http://www.ymlp.com/api/Contacts.GetUnsubscribed'
+         ).get :query => {'Key' => 'NNGAEPGKUKG2GNHWWJPK',
+                          'Username' => 'ericgj_rmu'
+                         }
          
-  EM.next_tick { 
-
-    http2 = conn.get(
-      '/api/Contacts.GetUnsubscribed?Key=NNGAEPGKUKG2GNHWWJPK&Username=ericgj_rmu'
-    )
-    
-    http2.callback do |response|
-      puts 'Callback fired from HttpClient2.get'
-      puts response.status
-      puts response.headers
-      puts response.content
-    end
-    
-  }
+  http.callback do
+    puts 'Callback fired from HttpRequest.new.get'
+    puts http.response_header.status
+    puts http.response_header.inspect
+    puts http.response
+  end
+  
+  http.errback do
+    puts 'failed'
+  end
   
   trap('INT') {
     EventMachine.stop if EventMachine.reactor_running?
