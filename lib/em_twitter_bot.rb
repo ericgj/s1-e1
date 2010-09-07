c2d428771ccce6ce9540d1afee9ccf855559e349
Eric Gjertsen <ericgj72@gmail.com>
2010-09-07 14:35:49 -0400

    require environment (oops)

--- lib/em_twitter_bot.rb
+++ lib/em_twitter_bot.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
 require File.join(File.dirname(__FILE__), 'em_twitter_bot', 'twitter_command_stream')
 require File.join(File.dirname(__FILE__), 'em_twitter_bot', 'twitter_reply_client')
+require File.join(File.dirname(__FILE__), 'em_twitter_bot', 'environment')
 require File.join(File.dirname(__FILE__), 'em_twitter_bot', 'bot')
